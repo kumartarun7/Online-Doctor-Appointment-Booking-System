6d3b6bbd657ae3778df36f0bb11578453d53ee89
kumartarun7 <kumartarun030803@gmail.com>
2024-07-25 20:45:34 +0530

    commit

--- app/api/getid/route.ts
+++ app/api/getid/route.ts
@@ -1,20 +1,25 @@
-import { getDataFromToken } from "@/helpers/getDataFromToken";
-import Jwt  from "jsonwebtoken";
-
-import { NextRequest,NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import Jwt from "jsonwebtoken";
 import { connect } from "@/dbconfig/dbconfig";
+
 connect();
-export async function GET(request:NextRequest){
-    try {
 
-        const token=request.cookies.get('token')?.value||''
-        if(token){ 
-            const decodedToken:any= Jwt.verify(token,process.env.TOKEN_SECRECT!);
-            return NextResponse.json(decodedToken);
-        }
-        else  return new Response(null)
+export async function GET(request: NextRequest) {
+  try {
+    const token = request.cookies.get('token')?.value || '';
 
-    } catch (error:any) {
-        throw new Error(error.message)
+    if (!token) {
+      return new NextResponse(JSON.stringify({ error: 'Token not provided' }), { status: 401 });
     }
+
+    try {
+      const decodedToken: any = Jwt.verify(token, process.env.TOKEN_SECRET!);
+      return NextResponse.json(decodedToken);
+    } catch (error: any) {
+      return new NextResponse(JSON.stringify({ error: 'Invalid token' }), { status: 401 });
+    }
+
+  } catch (error: any) {
+    return new NextResponse(JSON.stringify({ error: 'Internal Server Error' }), { status: 500 });
+  }
 }
